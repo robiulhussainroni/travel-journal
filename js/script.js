536331d9138cc24979f1d4a0061a9d61d585f253
Roni <rhroni09@gmail.com>
2026-05-17 03:30:39 +0600

    removed previous spaghetti comented code which I did not removed initially to keep reference

--- js/script.js
+++ js/script.js
@@ -8,47 +8,6 @@ const locationInfoLongitude = document.getElementById(
   "location__info--longitude",
 );
 
-// navigator.geolocation.getCurrentPosition(
-//   function (position) {
-//     const { latitude, longitude } = position.coords;
-//     const map = L.map("map").setView([latitude, longitude], 13);
-
-//     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//       attribution:
-//         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     }).addTo(map);
-
-//     L.marker([latitude, longitude])
-//       .addTo(map)
-//       .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-//       .openPopup();
-//   },
-//   function () {
-//     locationInfo.classList.remove("hidden");
-//   },
-// );
-
-// locationInfo.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   const latitude = +locationInfoLatitude.value;
-//   const longitude = +locationInfoLongitude.value;
-
-//   const map = L.map("map").setView([latitude, longitude], 13);
-
-//   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//     attribution:
-//       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//   }).addTo(map);
-
-//   L.marker([latitude, longitude])
-//     .addTo(map)
-//     .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-//     .openPopup();
-//   locationInfo.classList.add("hidden");
-// });
-
-// Organizing code
-
 // MapManager - manage everything related to map
 class MapManager {
   #map;
